93940a285613ff5096e3e271c1fb7585cfecbb05
terryfitech <eric@terryfi.com>
2026-08-16 11:51:11 -0400

    feat: register baseline security headers

--- src/acquisition_map/app.py
+++ src/acquisition_map/app.py
@@ -3,6 +3,7 @@
 from fastapi import FastAPI
 
 from acquisition_map.api.routes.health import router as health_router
+from acquisition_map.security.headers import SecurityHeadersMiddleware
 
 app = FastAPI(
     title="Acquisition Map API",
@@ -10,4 +11,5 @@ app = FastAPI(
     docs_url=None,
     redoc_url=None,
 )
+app.add_middleware(SecurityHeadersMiddleware)
 app.include_router(health_router)
